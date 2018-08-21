5660aef47cd0b1dc17f5be854708f8dfde4375db
manchejs0 <32222111+manchejs0@users.noreply.github.com>
2018-08-21 09:58:00 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import sqlite3
+import os
+
+
+
+app = Flask(__name__)
+
+conn = sqlite3.connect('mydb.db')
+
+c = conn.cursor()
+
+recentList = list(c.execute("SELECT * from data LIMIT 20"))
+
+
+listoutput = [i[1] for i in recentList]
+
+
+@app.route("/")
+
+def main():
+
+	
+
+	return render_template('index.html', data=listoutput)
+
+
+
+
+if __name__ == "__main__":
+
+	app.run()
+
+
