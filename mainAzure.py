d0db0f3b7c2a4c28e4e9dd881c38dff77e4ab7e7
manchejs0 <32222111+manchejs0@users.noreply.github.com>
2018-08-21 09:48:57 -0400

    Update mainAzure.py

--- mainAzure.py
+++ mainAzure.py
@@ -1,43 +1,79 @@
 import time
+import json
+import globalvar
 import directMethodSoftwareUpdate
 import requests
 import sys
 import serial
 import datetime
-import pymongo
 import dbFunctions
-import getFile
 import iothub_client
+
 from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
 from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
 
-#Create a serial connection with the correct protocol
-
-ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=5)
-
-#Global variable -- used for the software upgrade functionality
 
 
-# Create a client to interact with MongoDB
+"""
+First, we have to setup some things to be able to send messages to the IoT Hub.
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+We need a connection string, a protocol, a defined time for messages to timeout, and the message that we will later pass real
+values into.
 
-# Select the database where the appropriate values are located
-
-mydb = myclient["mydatabase"]
-
-# Select the table(collection) where the data is
-mycol = mydb["data"]
+"""
 
 
-CONNECTION_STRING = "HostName=jnj-diabpi.azure-devices.net;DeviceId=diabpi;SharedAccessKey=x/O4hgdG8ROelZsCj8lQshKFbDJCw8r0tFHaIkLX/pA="
-
+CONNECTION_STRING = "HostName=jnj-diabpi.azure-devices.net;DeviceId=diabpi;SharedAccessKey=uDGCy46qmPRxZHG5dQJ0zlRYVKc4RN5AGyQfc5sIfk0="
 PROTOCOL = IoTHubTransportProvider.MQTT
 MESSAGE_TIMEOUT = 100000
+MSG_TXT = "{\"heartRate\": %.2f, \"timestamp\": %s}"
+TWIN_CONTEXT = 0
 
-#Define the JSON message to send to IoT Hub.
 
-MSG_TXT = "{\"pulse\": %.2f, \"time\": %s}"
+
+
+"""
+
+Now, we get into the main body of the code
+
+"""
+
+def device_twin_callback(update_state, payload, user_context):
+    print("")
+    print( "Twin callback called with:")
+    print( "   updateStatus: %s" % update_state)
+    print("    payload: %s" % payload)
+    
+    json_acceptable_string = payload.replace("'", "\"")
+    
+    d = json.loads(json_acceptable_string)
+    
+    try:
+        
+        
+        globalvar.TELEMETRY_FREQ = d["desired"]["telemetryFrequency"]
+        
+    except:
+        
+        
+        if(int(d["telemetryFrequency"])%5 == 0 and int(d["telemetryFrequency"]) > 0):
+            
+            globalvar.TELEMETRY_FREQ = d["telemetryFrequency"]
+            
+        elif(int(d["telemetryFrequency"]) == 0 or int(d["telemetryFrequency"]) < 0):
+            
+            print("Can't accept negative values.  Frequency defaulted to 10.")
+            
+            
+            
+            globalvar.TELEMETRY_FREQ = 10
+            
+        else:
+            
+            globalvar.TELEMETRY_FREQ = 10
+   
+    
+    
 
 def send_confirmation_callback(message, result, user_context):
     print( "IoT Hub responded to message with status: %s" % (result) )
@@ -55,27 +91,34 @@ def iothub_client_telemetry_sample_run():
         print ( "IoT Hub device sending periodic messages, press Ctrl+C to exit")
         
         client.set_device_method_callback(directMethodSoftwareUpdate.device_method_callback, None)
+        client.set_device_twin_callback(device_twin_callback, TWIN_CONTEXT)
         
         while True:
-            #Build message with telemetry values.
+           
+            #Start reading values from the pulse sensor and storing it in a SQLite database.
             
-            mydict = { "pulse": int(ser.readline()), "timestamp":datetime.datetime.now(), "sent": 0}
-    
-            x = mycol.insert_one(mydict)
+            dbFunctions.storeValue()
             
-            dict = dbFunctions.getPulseJson()
+            #Retrieve the most recent value stored in the SQLite database.
+            list = dbFunctions.getValue()
+            
+            
     
-    
-            pulse = dict['pulse']
-    
-            timestamp = dict['timestamp']
-             
-            msg_txt_formatted = MSG_TXT % (float(pulse), timestamp)
+            pulse = list[1]
+
+            timestamp = list[2]
+            
+            #We have to add strings because otherwise it will cause an error with Thingworx
+            
+            timestamp1 = "'" + timestamp + "'"
+	    
+            msg_txt_formatted = MSG_TXT % (float(pulse), timestamp1)
             message = IoTHubMessage(msg_txt_formatted)
             
             print ("Sending message : %s" % message.get_string() )
             client.send_event_async(message, send_confirmation_callback, None)
-            time.sleep(10)
+            time.sleep(int(globalvar.TELEMETRY_FREQ))
+           
             
     except IoTHubError as iothub_error:
         print( "Unexpected error %s from IoTHub" % iothub_error )
